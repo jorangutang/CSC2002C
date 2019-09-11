27f26400fdf4c2815a25c7c4c86eff5260d7dabd
Jesse Smart <smart.jesse.smart@gmail.com>
2019-09-11 13:01:43 +0200

    one class

---
+++ SequentialWeather.java
@@ -0,0 +1,39 @@
+/**
+ * @author Jesse Smart
+ * SMRJES001
+ */
+public class SequentialWeather {
+
+    int[] timeSize;
+    float[] windLift;
+    int steps;
+    int xsize;
+    int ysize;
+
+
+    public SequentialWeather( int[] arr1, float[] arr2){
+        timeSize = arr1;
+        windLift = arr2;
+        steps = arr1[0];
+        xsize = arr1[1];
+        ysize = arr1[2];
+    }
+
+    public float[] windAv(){
+        float xOut = 0;
+        float yOut = 0;
+        int entries = xsize*ysize;
+        for(int i = 0; i < entries ; i++){
+            xOut += windLift[i*3];
+            yOut += windLift[i*3 +1];
+        }
+        xOut = xOut / entries;
+        yOut = yOut / entries;
+        float[] out = new float[]{xOut,yOut};
+        return out;
+    }
+
+    public float localAv()
+
+
+}
