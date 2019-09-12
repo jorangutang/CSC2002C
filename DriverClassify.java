0acb4253488c4657e6bbf2f14dbf91938fb5b85a
Jesse Smart <smart.jesse.smart@gmail.com>
2019-09-12 13:26:25 +0200

    clouddata adaoted

--- DriverClassify.java
+++ DriverClassify.java
@@ -1,6 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+
+
 /**
  * @author Jesse Smart
  * SMRJES001
  */
 public class DriverClassify {
+
+    public static int x,y,t;
+    public static float[][][] U;
+    public static int[][][] clouds;
+
+    public static void main(String[] args) {
+
+        //read data
+        //create objects
+
+        String outputfile;
+
+        //use args
+
+        //call readdata
+
+        //set dims xyt
+
+        //cuttoffs
+
+        //loop for sums and times
+
+        //retrun runtime
+
+        //windaverage
+
+
+    }
+
 }
---
+++ Vector.java
@@ -0,0 +1,14 @@
+/**
+ * @author Jesse Smart
+ * SMRJES001
+ */
+public class Vector {
+    
+    float x;
+    float y;
+    
+    public Vector(float x, float y){
+        this.x =x;
+        this.y =y;
+    }
+}
